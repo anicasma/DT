df7f2f7e53a34d37351c00dbe3a87ca7554fdb21
anicasma <34836446+anicasma@users.noreply.github.com>
2019-03-27 14:15:35 +0100

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,26 +1,8 @@
 #include<iostream>
-#include<string>
-#include<memory>
 
-
-class Entity{
-   public: 
-      Entity(){
-         std::cout<<"Kreiran Entity!"<<std::endl;
-      }
-      ~Entity(){
-         std::cout<<"Izbrisan Entity"<<std::endl;
-      } 
-      void Print(){
-         //Empty
-      }
-};
 
 int main(){
 
-   std::unique_ptr<Entity> entity = std::make_unique<Entity>();
-   entity->Print;
-
-std::cin.get();
+std::cout<<"Zdravo"<<std::endl;
 
 }
